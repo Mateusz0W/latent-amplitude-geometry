917a746851c5f990668f7495b8dd2a0a788ab8bf
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2026-08-14 14:06:38 +0200

    update Early stopping implementation

--- src/training/early_stopping.py
+++ src/training/early_stopping.py
@@ -1,26 +1,32 @@
+import copy
+
 class EarlyStopping:
-    def __init__(self, patience=5, delta=0):
+    def __init__(self, patience=5, min_delta=0, verbose=False):
         self.patience = patience
-        self.delta = delta
-        self.best_score = None
-        self.early_stop = False
+        self.min_delta = min_delta
+        self.verbose = verbose
         self.counter = 0
-        self.best_model_state = None
+        self.best_loss = None
+        self.best_model = None
+        self.early_stop = False
 
     def __call__(self, val_loss, model):
-        score = -val_loss
-
-        if self.best_score is None:
-            self.best_score = score
-            self.best_model_state = model.state_dict()
-        elif score < self.best_score + self.delta:
+        if self.best_loss is None:
+            self.best_loss = val_loss
+            self.save_checkpoint(model)
+        elif val_loss < self.best_loss - self.min_delta:
+            if self.verbose:
+                print(f"Validation loss decreased ({self.best_loss:.6f} --> {val_loss:.6f}). Saving model...")
+            self.best_loss = val_loss
+            self.save_checkpoint(model)
+            self.counter = 0
+        else:
             self.counter += 1
+            if self.verbose:
+                print(f"EarlyStopping counter: {self.counter} out of {self.patience}")
+
             if self.counter >= self.patience:
                 self.early_stop = True
-        else:
-            self.best_score = score
-            self.best_model_state = model.state_dict()
-            self.counter = 0
 
-    def load_best_model(self, model):
-        model.load_state_dict(self.best_model_state)
+    def save_checkpoint(self, model):
+        self.best_model = copy.deepcopy(model.state_dict())
